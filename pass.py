22b577f9e5a098cf895358e3405a2a06335b1055
Tang9511 <31895573+Tang9511@users.noreply.github.com>
2018-01-16 00:05:39 +0800

    0.1

--- pass.py
+++ pass.py
@@ -10,6 +10,9 @@ import numpy as  np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+
+
 train=pd.read_csv('train.csv')
 test=pd.read_csv('test.csv')
 train.info()                     # information of train set 
@@ -17,8 +20,10 @@ train.info()                     # information of train set
 print(train.Survived.value_counts())
 
 fig = plt.figure()
-fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
 Survived_0 = train.Pclass[train.Survived == 0].value_counts()
 Survived_1 = train.Pclass[train.Survived == 1].value_counts()
 df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
@@ -28,15 +33,15 @@ plt.xlabel(u"乘客等级")
 plt.ylabel(u"人数") 
 plt.show()
 
+#print(train.Age.value_counts())
+age=train.Age.value_counts()
+print(age.sort_index())
+sur=train.Age[train.Survived == 1].value_counts()
+mature=train.Age[(train.Age <= 30) & (train.Age >=18)]
 
+print(sur.sort_index())
+print(mature.sort_index())
+print(train.Age)
 
-'''
-labels=np.loadtxt('digits4000_digits_labels.txt')
-testset=np.loadtxt('digits4000_testset.txt')
-
-challengelabel=np.loadtxt('cdigits_digits_labels.txt')
-challengedigits_vec=np.loadtxt('cdigits_digits_vec.txt')
-
-print("hello,python")
-digits=np.asmatrix(digits_vec)
-'''
+df1=pd.DataFrame(train.Age)
+df2=df1.dropna(axis=0,how='any') # 0: 对行进行操作; 1: 对列进行操作'any': 只要存在 NaN 就 drop 掉; 'all': 必须全部是 NaN 才 drop 
