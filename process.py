89fbc7715b60cb03b7ac853fb7b93ff1e192074e
Tang9511 <31895573+Tang9511@users.noreply.github.com>
2018-01-24 16:32:36 +0800

    0.3
    Using logistic Regression model. Score is 0.75598.

--- process.py
+++ process.py
@@ -8,15 +8,49 @@ Created on Sun Jan 21 22:35:13 2018
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 from setmissingval import set_missing_age, set_missing_embarkinfo
 
 train=pd.read_csv('train.csv')
 test=pd.read_csv('test.csv')
+sub=pd.read_csv('gender_submission.csv')
+full_data=[train,test]
 train.info()                     # information of train set 
 
-train=set_missing_age(train)  #补全age缺失值。用randomforest做回归拟合
+[train,test]=set_missing_age(train,test)  #补全age缺失值。用randomforest做回归拟合
 train=set_missing_embarkinfo(train)  #补全embarked。只缺了两个，补了占比做多的登陆信息
+test=set_missing_embarkinfo(test)
+'''
+ # Mapping Age
+train.loc[ train['Age'] <= 16, 'Age'] = 0
+train.loc[(train['Age'] > 16) & (train['Age'] <= 32), 'Age'] = 1
+train.loc[(train['Age'] > 32) & (train['Age'] <= 48), 'Age'] = 2
+train.loc[(train['Age'] > 48) & (train['Age'] <= 64), 'Age'] = 3
+train.loc[ train['Age'] > 64, 'Age'] = 4
+'''
+
+for dataset in full_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
 
 dummies_Embarked = pd.get_dummies(train['Embarked'], prefix= 'Embarked')
 dummies_Sex = pd.get_dummies(train['Sex'], prefix= 'Sex')
 dummies_Pclass = pd.get_dummies(train['Pclass'], prefix= 'pclass')
+df_train=pd.concat([dummies_Embarked,dummies_Sex,dummies_Pclass,train['Age']],axis=1)
+label=train['Survived']
+
+dummies_Embarked = pd.get_dummies(test['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(test['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(test['Pclass'], prefix= 'pclass')
+df_test=pd.concat([dummies_Embarked,dummies_Sex,dummies_Pclass,test['Age']],axis=1)
+
+lrg = LogisticRegression()
+lrg.fit(df_train,label)
+predictions=lrg.predict(df_test)
+print(lrg.score(df_test,sub.loc[:,'Survived']))
+
+result = pd.DataFrame({'PassengerId':test['PassengerId'].as_matrix(), 'Survived':predictions})
+result.to_csv('results.csv', header=True,index=False)
--- setmissingval.py
+++ setmissingval.py
@@ -8,14 +8,15 @@ Created on Tue Jan 16 17:14:31 2018
 
 from sklearn.ensemble import RandomForestRegressor
 
-def set_missing_age(df):
+def set_missing_age(df,test):
     #乘客分成已知年龄和未知年龄两部分
     age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
     known_age = age_df[age_df.Age.notnull()].as_matrix()
     unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+    age_test=test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+    unknown_testage=age_test[age_test.Age.isnull()].as_matrix()
     # y即目标年龄
     y = known_age[:, 0]
-
     # X即特征属性值
     X = known_age[:, 1:]
     regr = RandomForestRegressor(max_depth=2, random_state=0,n_estimators=50)
@@ -23,7 +24,9 @@ def set_missing_age(df):
     predictedAges = regr.predict(unknown_age[:, 1::])
     df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges 
     #print(regr.feature_importances_)
-    return df
+    test_age=regr.predict(unknown_testage[:,1::])
+    test.loc[ (test.Age.isnull()), 'Age' ] = test_age
+    return df,test
     
 def set_missing_embarkinfo(df):
     df['Embarked'] = df['Embarked'].fillna('S')
